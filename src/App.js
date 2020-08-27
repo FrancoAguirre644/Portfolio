2372b94418dbaee3846122765ebda2ad011c9558
Franco <FrancoAguirre644@gmail.com>
2020-08-27 12:39:19 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap-css-only/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
+
+function App() {
+  return (
+    <div >
+
+    </div>
+  );
+}
+
+export default App;
